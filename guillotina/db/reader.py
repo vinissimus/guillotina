da0432d98fc8c1edf28daa0e64799f1927a4a7bc
Jordi Masip <jordi@masip.cat>
2022-05-17 15:15:14 +0200

    Deserialize pickles in a asyncio executor

--- guillotina/db/reader.py
+++ guillotina/db/reader.py
@@ -1,11 +1,17 @@
 from guillotina.db.orm.interfaces import IBaseObject
+from guillotina.utils import run_async
 
 import pickle
 import typing
 
 
-def reader(result: dict) -> IBaseObject:
-    obj = typing.cast(IBaseObject, pickle.loads(result["state"]))
+async def reader(result: dict) -> IBaseObject:
+    state = result["state"]
+    if len(state) > 25000:
+        o = await run_async(pickle.loads, state)
+    else:
+        o = pickle.loads(state)
+    obj = typing.cast(IBaseObject, o)
     obj.__uuid__ = result["zoid"]
     obj.__serial__ = result["tid"]
     obj.__name__ = result["id"]
